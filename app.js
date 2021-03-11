ec8f5ae323fceb5c186cd2aa238438bf52979651
Sangman Yoo <sangmanyoo@gmail.com>
2021-03-12 06:29:37 +0900

    index.html

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWith = 2.5;
 
 let painting = false;
 
-function stopPainting() {
+function stopPainting(event) {
   painting = false;
 }
 
@@ -10,23 +17,23 @@ function onMouseMove(event) {
   console.log(event);
   const x = event.offsetX;
   const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseDown(event) {
+function startPainting(event) {
   painting = true;
-}
-
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function onMouseLeave(event) {
-  stopPainting();
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", onMouseLeave);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
